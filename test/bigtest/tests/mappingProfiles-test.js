679cc3a2d60af75c1418de67dfffaaed2100f9c1
Yevhenii Maltsev <yevhenii_maltsev@epam.com>
2020-04-30 16:47:06 +0300

    STDTC-9: Updated mapping profiles pane according to changes in shared components

--- test/bigtest/tests/mappingProfiles-test.js
+++ test/bigtest/tests/mappingProfiles-test.js
@@ -5,13 +5,13 @@ import {
   it,
 } from '@bigtest/mocha';
 
-import { MappingProfilesInteractor } from '@folio/stripes-data-transfer-components/interactors';
+import { SearchAndSortInteractor } from '@folio/stripes-data-transfer-components/interactors';
 import stripesDataTransferTranslations from '@folio/stripes-data-transfer-components/translations/stripes-data-transfer-components/en';
 import translations from '../../../translations/ui-data-export/en';
 
 import { setupApplication } from '../helpers';
 
-const mappingProfiles = new MappingProfilesInteractor();
+const mappingProfiles = new SearchAndSortInteractor();
 
 describe('Field mapping profiles settings', () => {
   setupApplication();
@@ -25,18 +25,18 @@ describe('Field mapping profiles settings', () => {
   });
 
   it('should place headers in correct order', () => {
-    expect(mappingProfiles.profilesList.headers(0).text).to.equal(stripesDataTransferTranslations.name);
-    expect(mappingProfiles.profilesList.headers(1).text).to.equal(stripesDataTransferTranslations.folioRecordType);
-    expect(mappingProfiles.profilesList.headers(2).text).to.equal(translations.format);
-    expect(mappingProfiles.profilesList.headers(3).text).to.equal(stripesDataTransferTranslations.updated);
-    expect(mappingProfiles.profilesList.headers(4).text).to.equal(stripesDataTransferTranslations.updatedBy);
+    expect(mappingProfiles.searchResults.list.headers(0).text).to.equal(stripesDataTransferTranslations.name);
+    expect(mappingProfiles.searchResults.list.headers(1).text).to.equal(stripesDataTransferTranslations.folioRecordType);
+    expect(mappingProfiles.searchResults.list.headers(2).text).to.equal(translations.format);
+    expect(mappingProfiles.searchResults.list.headers(3).text).to.equal(stripesDataTransferTranslations.updated);
+    expect(mappingProfiles.searchResults.list.headers(4).text).to.equal(stripesDataTransferTranslations.updatedBy);
   });
 
   it('should display correct fields values', () => {
-    expect(mappingProfiles.getCellContent(0, 0)).to.equal('AP Holdings 1');
-    expect(mappingProfiles.getCellContent(0, 1)).to.equal(stripesDataTransferTranslations['recordTypes.holdings']);
-    expect(mappingProfiles.getCellContent(0, 2)).to.equal('MARC');
-    expect(mappingProfiles.getCellContent(0, 3)).to.equal('12/4/2018');
-    expect(mappingProfiles.getCellContent(0, 4)).to.equal('Donald S');
+    expect(mappingProfiles.searchResults.getCellContent(0, 0)).to.equal('AP Holdings 1');
+    expect(mappingProfiles.searchResults.getCellContent(0, 1)).to.equal(stripesDataTransferTranslations['recordTypes.holdings']);
+    expect(mappingProfiles.searchResults.getCellContent(0, 2)).to.equal('MARC');
+    expect(mappingProfiles.searchResults.getCellContent(0, 3)).to.equal('12/4/2018');
+    expect(mappingProfiles.searchResults.getCellContent(0, 4)).to.equal('Donald S');
   });
 });
