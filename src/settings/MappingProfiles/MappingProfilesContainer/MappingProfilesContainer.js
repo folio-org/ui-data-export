3d40d30bbec641035d096006cad3a880a5083cdf
Viktor Soroka <victor_soroka1@epam.com>
2020-05-01 20:33:50 +0300

    UIDEXP-96: Navigate to full screen empty form upon creating new mapping profile

--- src/settings/MappingProfiles/MappingProfilesContainer/MappingProfilesContainer.js
+++ src/settings/MappingProfiles/MappingProfilesContainer/MappingProfilesContainer.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import {
+  match as matchShape,
+  history as historyShape,
+  location as locationShape,
+} from 'react-router-prop-types';
+
+import { Route } from '@folio/stripes/core';
+import {
   MappingProfiles,
   getMappingProfilesColumnProperties,
   DEFAULT_MAPPING_PROFILES_COLUMNS,
   getMappingProfilesItemFormatter,
 } from '@folio/stripes-data-transfer-components';
 
+import { MappingProfilesForm } from '../MappingProfilesForm';
 import tempData from './tempData';
 
 const customProperties = {
@@ -22,33 +30,54 @@ const visibleColumns = [
   DEFAULT_MAPPING_PROFILES_COLUMNS.UPDATED_BY,
 ];
 
-const MappingProfilesContainer = () => {
+const MappingProfilesContainer = ({
+  history,
+  match,
+  location,
+}) => {
   return (
-    <MappingProfiles
-      // TODO: temp solution to simulate mutators and resources that should be removed after integration with backend
-      parentResources={{
-        query: {
-          sort: new URLSearchParams(window.location.search).get('sort') || '',
-          query: new URLSearchParams(window.location.search).get('query') || '',
-        },
-        mappingProfiles: {
-          records: tempData.profiles,
-          hasLoaded: true,
-          isPending: false,
-          other: { totalRecords: 4 },
-        },
-      }}
-      parentMutator={{
-        resultCount: { replace: () => { } },
-        resultOffset: { replace: () => { } },
-      }}
-      formatter={getMappingProfilesItemFormatter({ format: record => record.format })}
-      {...getMappingProfilesColumnProperties({
-        visibleColumns,
-        customProperties,
-      })}
-    />
+    <>
+      <MappingProfiles
+        // TODO: temp solution to simulate mutators and resources that should be removed after integration with backend
+        parentResources={{
+          query: {
+            sort: new URLSearchParams(window.location.search).get('sort') || '',
+            query: new URLSearchParams(window.location.search).get('query') || '',
+          },
+          mappingProfiles: {
+            records: tempData.profiles,
+            hasLoaded: true,
+            isPending: false,
+            other: { totalRecords: 4 },
+          },
+        }}
+        parentMutator={{
+          resultCount: { replace: () => { } },
+          resultOffset: { replace: () => { } },
+        }}
+        formatter={getMappingProfilesItemFormatter({ format: record => record.format })}
+        {...getMappingProfilesColumnProperties({
+          visibleColumns,
+          customProperties,
+        })}
+      />
+      <Route
+        path={`${match.path}/create`}
+        render={props => (
+          <MappingProfilesForm
+            {...props}
+            onCancel={() => history.push(`${match.path}${location.search}`)}
+          />
+        )}
+      />
+    </>
   );
+};
+
+MappingProfilesContainer.propTypes = {
+  match: matchShape.isRequired,
+  history: historyShape.isRequired,
+  location: locationShape.isRequired,
 };
 
 export default MappingProfilesContainer;
