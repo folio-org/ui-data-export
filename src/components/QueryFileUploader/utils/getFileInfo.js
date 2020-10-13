f3cc759a023676465cec5ef05652c96dc63cfe71
yelyzavetakhokhlova <yelyzaveta_khokhlova@epam.com>
2020-10-13 11:23:55 +0300

    UIDEXP-156: Replace substring with slice

--- src/components/QueryFileUploader/utils/getFileInfo.js
+++ src/components/QueryFileUploader/utils/getFileInfo.js
@@ -2,7 +2,7 @@ import { getFileExtension } from '@folio/stripes-data-transfer-components';
 import { SUPPORTED_FILE_EXTENSIONS } from '../../../utils/constants';
 
 export const getFileInfo = file => {
-  const fileType = getFileExtension(file).substring(1);
+  const fileType = getFileExtension(file).slice(1);
 
   return {
     isTypeSupported: Boolean(SUPPORTED_FILE_EXTENSIONS[fileType.toUpperCase()]),
