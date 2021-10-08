754fbce40b51661bea21c1ae75c4fc5c1b7143ce
Vlad Samets <uladislau_samets@epam.com>
2021-10-08 11:31:45 +0300

    fit tests

--- test/bigtest/interactors/jobs/JobsAccordionInteractor.js
+++
@@ -1,9 +0,0 @@
-import {
-  interactor,
-  scoped,
-} from '@bigtest/interactor';
-
-@interactor
-export default class JobsAccordionInteractor {
-  title = scoped('[data-test-jobs-accordion-title]');
-}
--- test/bigtest/tests/units/settings/MappingProfileDetails/interactors/TransformationsInteractor.js
+++
@@ -1,8 +0,0 @@
-import { interactor } from '@bigtest/interactor';
-
-import MultiColumnListInteractor from '@folio/stripes-components/lib/MultiColumnList/tests/interactor';
-
-@interactor
-export class TransformationsInteractor {
-  list = new MultiColumnListInteractor('#mapping-profile-transformations-list');
-}
