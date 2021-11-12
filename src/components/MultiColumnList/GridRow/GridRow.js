67b0a235800229aba714dcfdb42f07a2c67d865a
uladzislau_kutarkin <uladzislau_kutarkin@epam.com>
2021-11-12 11:09:02 +0300

    UIDEXP-214: Add aria-label in mapping profile transformations list

--- src/components/MultiColumnList/GridRow/GridRow.js
+++ src/components/MultiColumnList/GridRow/GridRow.js
@@ -6,6 +6,12 @@ import { GridCell } from '../GridCell';
 
 import css from './GridRow.css';
 
+const arialLabelString = obj => Object.values(obj).reduce((acc, value) => {
+  acc.push(value);
+
+  return acc;
+}, []).join(' ');
+
 export const GridRow = React.memo(({
   data,
   index,
@@ -17,6 +23,7 @@ export const GridRow = React.memo(({
       data-row-inner
       className={classNames(css.mclRow, { [css.mclIsOdd]: !(index % 2) })}
       style={style}
+      aria-label={arialLabelString(data.contentData[index])}
     >
       {data.visibleColumns.map(column => (
         <GridCell
