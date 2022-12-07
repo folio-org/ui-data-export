6a8fa01207e04aa920899b6b7ac04b7d67050d59
Vlad Samets <uladislau_samets@epam.com>
2022-12-07 17:28:58 +0600

    UIDEXP-296  Increase unit test code coverage for ui-data-export

---
+++ src/settings/MappingProfiles/MappingProfileDetailsRoute/MappingProfileDetailsRoute.test.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+import '../../../../test/jest/__mock__';
+import { renderWithIntl } from '@folio/stripes-data-transfer-components/test/jest/helpers';
+import { screen } from '@testing-library/react';
+import { MappingProfileDetailsRoute } from '.';
+
+import { translationsProperties } from '../../../../test/helpers';
+
+jest.mock('../MappingProfileDetails' ,() => ({
+  MappingProfileDetails: () => (<div>MappingProfileDetails</div>)
+}))
+
+const setupMappingProfileDetailsRoute = ({
+  resources = {
+    mappingProfile: {},
+    jobProfiles: [],
+  },
+  allTransformations = [],
+  mutator = { mappingProfile: { DELETE: jest.fn() } },
+  history = {},
+  match = { params: {} },
+  location = {},
+  onCancel = jest.fn(),
+}= {}) => {
+  renderWithIntl(
+    <MappingProfileDetailsRoute
+      resources= {resources}
+      allTransformations={allTransformations}
+      mutator={mutator}
+      history={history}
+      match={match}
+      location={location}
+      onCancel={onCancel}
+    />,
+    translationsProperties
+  )
+};
+
+describe('MappingProfileDetailsRoute', () => {
+  it('should display mapping profile details route', async () => {
+    setupMappingProfileDetailsRoute();
+
+    expect(screen.getByText('MappingProfileDetails')).toBeVisible();
+
+  });
+
+});
