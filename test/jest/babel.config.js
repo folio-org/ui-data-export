dcfb29fa29670ba659b666b221bf2846b49b41b0
vashjs <vadym.shchekotilin@gmail.com>
2023-06-22 16:18:47 +0300

    replace proposal with transform for babel modules

--- test/jest/babel.config.js
+++ test/jest/babel.config.js
@@ -8,7 +8,7 @@ module.exports = {
       '@babel/plugin-proposal-decorators',
       { legacy: true },
     ],
-    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-transform-class-properties',
     '@babel/plugin-transform-runtime',
   ],
 };
