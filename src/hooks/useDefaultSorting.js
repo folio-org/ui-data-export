6d68b21ee1c032e237810603fefbe6d9dec1fe7d
Vadym Shchekotilin <86330150+vashjs@users.noreply.github.com>
2024-03-25 12:08:41 +0100

    UIDEXP-366 Error message is displayed when go from Data export Settings to View All page (#447)

--- src/hooks/useDefaultSorting.js
+++ src/hooks/useDefaultSorting.js
@@ -2,15 +2,21 @@ import { useEffect } from 'react';
 import { buildSearch } from '@folio/stripes-acq-components';
 import { useHistory } from 'react-router-dom';
 
+// eslint-disable-next-line import/prefer-default-export
 export const useDefaultSorting = () => {
   const history = useHistory();
 
   useEffect(() => {
+    history.replace({
+      search: `${buildSearch({
+        sort: 'name'
+      }, history.location.search)}`,
+    });
+
+    // clear search params on unmount to avoid conflicts with other components
     return () => {
       history.replace({
-        search: `${buildSearch({
-          sort: 'name'
-        }, history.location.search)}`,
+        search: '',
       });
     };
   }, []);
