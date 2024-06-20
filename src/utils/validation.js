dfd0489d664154929629b37ee68358959d2b9fea
Uladzislau_Kutarkin <uladzislau_kutarkin@epam.com>
2024-06-20 15:49:44 +0400

    UIDEXP-378: replace regexp for security reasons

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -17,7 +17,7 @@ export const fieldSuppression = (value) => {
     return undefined;
   }
 
-  const trimmedValue = value?.replace(/,*$/, '');
+  const trimmedValue = value?.split(',').filter(Boolean).join(',');
 
   const fields = trimmedValue.split(',').map(field => field.trim());
 
